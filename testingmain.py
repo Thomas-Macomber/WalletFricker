7fde5d3411b51b2369af7df46326251720dbd6b9
Thomas-Macomber <Thomas.Macomber@gmail.com>
2018-02-10 14:34:02 -0500

    Went through the code and rewrote the parts I anticipate needing to test

--- testingmain.py
+++ testingmain.py
@@ -12,6 +12,11 @@ import base64
 import sys
 import json
 
+#string define for the URL
+urlPub = "https://www.cryptopia.co.nz/api/"
+urlPri = "https://www.cryptopia.co.nz/Api/"
+running = true
+
 #Okay so I don't understand this funftion definition but it seems important (its the main query funct.)
 def api_query (method, req = None ):
     #like WTF is this !
@@ -22,8 +27,28 @@ def api_query (method, req = None ):
     public_set = set([ "GetCurrencies", "GetTradePairs", "GetMarkets", "Get Market", "GetMarketHistory"])
     #the array/set of all of the private API Commands
     private_set = set([ "GetBalance", "GetDepositAddress", "GetOpenOrders", "GetTradeHistory", "GetTransactions", "SubmitTrade", "CancelTrade", "SubmitTip"])
+    #Tells the function how to handle querys in the public array
+    if method in public_set:
+        #Sets the current used URL (only import if the Api vs api matters)
+        url = urlPub + method
+        #If there is a specific request (i.e. GetMarket singular)
+        if req:
+            #no fucking clue
+            for param in req:
+                #adds a slash and the neccessary params
+                url += "/" +str( param )
+        #Passes data through r cause we lazy
+        r = requests.get( url )
+    #Tells the funtion how to handle querys in the private array (with authentications)
+    elif method in private_set:
+        url = urlPri + method
 
+        #leaving this blank for a while until I can successfully log the public queries
 
+    #Finishes the pass through with r
+    response = r.text
+    #Gives you that sweet fucking mail
+    print "You Got Mail!!! " + response
 
 #Anything we need to run once at the begining of the program
 def setup():
@@ -36,3 +61,9 @@ def setup():
 
 #Anything we want to run indefinitely
 def loop ():
+    print api_query("GetMarket", [4405])
+
+#starts the non terminating loop
+while running:
+    #runs loop (Woah)
+    loop()
