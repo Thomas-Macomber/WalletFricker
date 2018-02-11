0547bd965e9097466a08725b0a2368845bb9d588
Thomas-Macomber <Thomas.Macomber@gmail.com>
2018-02-11 15:30:38 -0500

    Added them fucking comments boi

--- LogTest.py
+++ LogTest.py
@@ -4,10 +4,10 @@ import time
 import os
 
 running = 1
-CryptoHopperName = "We Need a Name"
+CryptoHopperName = "Wallet Fucker"
 version = "Indev 0.0.1"
 last96 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-last15 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+last30 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 lastPrice = 0
 
 print("\nScript to find the current information about crypto.\nType 'help' to view valid commands.")
@@ -60,6 +60,8 @@ def get_markets():
     else:
         bot()
 
+#basically GetMarkets except returns the value to the exterior function rather than printing it
+#MIGHT BE FUCKED UP
 def retrievePrice( pairString ):
     url = "https://www.cryptopia.co.nz/api/GetMarkets"
     r = requests.get(url)
@@ -94,29 +96,40 @@ def bot():
     else:
         print("\nPlease enter a valid command.")
 
+#The main screen print with some basic formatting logic
 def printLogScreen( ticker, lastPrice, SMA, lastBuyPrice, lastBuyQuant, waitBuy, lastSellPrice, lastSellQuant, waitSell, momentum):
+    #defines the coins
     slashIndex = ticker.find("/")
     coin1 = ticker[:slashIndex]
     coin2 = ticker[slashIndex+1:]
+
+    #determines the order status and which values should be N/A
+    #if we have a pending buy order we want to know how much it's trying to buy
     if waitBuy == 1:
         buyIn = lastBuyPrice
         buyQ = lastBuyQuant
         sellOut = "N/A "
         sellQ = "N/A "
         status = "Pending Buy Order"
+    #if we have a pending sell order we want to know how much its selling for and also what the previous purchase was for reference
     elif waitSell == 1:
         buyIn = lastBuyPrice
         buyQ = lastBuyQuant
         sellOut = lastSellPrice
         sellQ = lastSellQuant
         status = "Pending Sell Order"
+    #if we have no pending orders its just scanning and we dont give a fuck
     else:
         buyIn = "N/A "
         buyQ = "N/A "
         sellOut = "N/A "
         sellQ = "N/A "
         status = "Scanning Market... "
+
+    #clear the screen for clean look
     os.system('cls')
+
+    #prints the screen
     print ( CryptoHopperName + "___" + version )
     print ( "Logging: " + ticker )
     print ( "Last Price: " + str(lastPrice) )
@@ -128,49 +141,74 @@ def printLogScreen( ticker, lastPrice, SMA, lastBuyPrice, lastBuyQuant, waitBuy,
     print ( "Sell: " + str(sellQ) + coin1 )
     print ( "   @ " + str(sellOut) + coin2 )
 
+#This function floods the last96 with a set value that the user determines to be near the currentSMA for a baseline
 def set96( pairString ):
     currentSMA = input( "Please input current market SMA for: " + pairString + ": ")
     type(currentSMA)
     for i in range( 0, 96 ):
         last96[i] = float(currentSMA)
 
+#This functions bumps all of the last96 values foward one and adds the lastPrice to the 0th index
 def foward96( lastPrice ):
     for i in range( 95, 0, -1 ):
         #print(i)
         last96[ i ] = last96 [ i - 1]
     last96[0] = lastPrice
 
-def foward15( lastPrice ):
-    for i in range( 14, 0, -1 ):
+#This funtion mirrors foward96 but for last30
+def foward30( lastPrice ):
+    for i in range( 29, 0, -1 ):
         #print(i)
-        last15[ i ] = last15[ i - 1]
-    last15[0] = lastPrice
+        last30[ i ] = last30[ i - 1]
+    last30[0] = lastPrice
 
-
+#The main log
 def log( pairString ):
+    #The last 96 should only update every 15 minutes, this keeps track of progress
     counter96 = 0
+    #If we're waiting to confirm a buy ORDER
+    #NOT YET IMPLIMENTED
     waitBuy = 0
+    #If we're waiting to confirm a sell ORDER
+    #NOT YET IMPLIMENTED
     waitSell = 0
+    #sets the SMA to give a baseline
+    set96( pairString )
+    #Starts the loop of unending refresh
     while 1 < 2:
+        #grabs the lastprice
         lastPrice = retrievePrice( pairString )
-        print(lastPrice)
-        foward15( lastPrice )
+        #moves the 30 values stored in last30 foward by 1 adding in the current price
+        foward30( lastPrice )
+
+        #refreshes the SMA
         sum96 = 0
         for i in range(0,len(last96)):
             sum96 += last96[i]
         SMA = float(sum96) / float(len(last96))
-        if counter96 == 29:
-            counter96 = 0
-            sum15 = 0
-            for i in range(0,len(last15)):
-                sum15 += last15[i]
-            avg15 = float(sum15) / float(len(last15))
-            foward96( sum(last15)/float(15) )
-        printLogScreen( pairString, lastPrice, SMA, 0, 0, waitBuy, 0, 0, waitSell, 0)
-        time.sleep(30)
-        counter96 += 1
 
-set96( "HUSH/BTC" )
+        #if the counter has hit 29 then we have gone through 30 chunks of 30 seconds (15 min)
+        #this starts an update of last 96, the less precise list over a longer period
+        if counter96 == 29:
+            #resets counter
+            counter96 = 0
+
+            #averages the last 15 minutes
+            sum30 = 0
+            for i in range(0,len(last30)):
+                sum30 += last30[i]
+            avg30 = float(sum30) / float(len(last30))
+
+            #slaps that bitch into the last96 list
+            foward96( avg30 )
+
+        #prints the main screen
+        printLogScreen( pairString, lastPrice, SMA, 0, 0, waitBuy, 0, 0, waitSell, 0)
+        #increments the counter
+        counter96 += 1
+        #waits 30 seconds (our current refresh rate)
+        time.sleep(30)
+
 log( "HUSH/BTC" )
 
 
