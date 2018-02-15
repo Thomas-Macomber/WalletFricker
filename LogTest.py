b59290b91c6523ed88a101401f161258cd603d6e
Sean Glasgow <seanmaple@watermist.us>
2018-02-15 12:18:00 -0500

    Added classes and cleaned up. New import, check comments in LogTest.py

--- LogTest.py
+++ LogTest.py
@@ -2,6 +2,12 @@ import requests
 import sys
 import time
 import os
+#This has been imported in case you want to call anything from the other file. That way we don't have to rewrite it here.
+#For example if you wanted to call the old get_market function you'd type "public_api.get_market()" and it would call it.
+#Check out simpleHardCode.py to see how the class is set up.
+import simpleHardCode
+
+#For the sake of possibly calling functions in other files later I recommend you construct some classes and organize them.
 
 running = 1
 CryptoHopperName = "Wallet Fucker"
@@ -9,13 +15,6 @@ version = "Indev 0.0.1"
 last96 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 last30 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 lastPrice = 0
-
-print("\nScript to find the current information about crypto.\nType 'help' to view valid commands.")
-
-#I'm going to make some changes to the methods. Primary so that we have specific methods for each API command instead
-#of having one large method that does everything. Maybe I'll clean it up later but this is mostly because
-#the one large method last time was fucking everything up. So I'm going to rename this method "api_query" to "get_market"
-
 
 #Added color class. Each variable is a string representation that you need to enter to change the color of text.
 #I made a change to printLogScreen to utilize colors for the first printed line. Feel free to check it to see implementation and run the program.
@@ -28,52 +27,6 @@ class color:
     CYAN = "\033[1;36;40m"
     BLACK = "\033[1;30;40m"
     WHITE = "\033[1;37;40m"
-
-
-def get_market():
-    tpiString = input("Please enter the TradePairId you would like to search for: ")
-    type(tpiString)
-    url = "https://www.cryptopia.co.nz/api/" + "GetMarket/" + tpiString
-    r = requests.get(url)
-    rString = r.text
-    lastIndex = rString.find("LastPrice")
-    buyIndex = rString.find("BuyVolume")
-    if(lastIndex != -1):
-        slicePrice = rString[lastIndex+11:buyIndex-2]
-        print("Last sold price of trade pair " + tpiString + " is: "  + slicePrice)
-    elif(lastIndex == -1):
-        slicePrice = rString[lastIndex+60:buyIndex-10]
-        print("TradePairId: " + slicePrice + "does not exist.")
-    continueString = input("\nWould you like to input another TradePairId? (y/n): ")
-    type(continueString)
-    if(continueString == "y"):
-        get_market()
-    else:
-        bot()
-
-def get_markets():
-    pairString = input("\nPlease enter a pair in the format COIN/COIN to see their exchange rate: ")
-    type(pairString)
-    slashIndex = pairString.find("/")
-    coin1 = pairString[:slashIndex]
-    coin2 = pairString[slashIndex+1:]
-    url = "https://www.cryptopia.co.nz/api/GetMarkets"
-    r = requests.get(url)
-    rString = r.text
-    pairIndex = rString.find(pairString)
-    if(pairIndex != -1):
-        formattedText = rString[pairIndex:pairIndex+200]
-        lastPriceIndex = formattedText.find("LastPrice")
-        slicePrice = formattedText[(lastPriceIndex+11):(lastPriceIndex+20)]
-        print("The exchange rate for " + coin1 + " to " + coin2 + " is: " + slicePrice +"\nOr 1 " + coin1 + " to " + slicePrice + " " + coin2)
-    elif(pairIndex == -1):
-        print("The pair: " + pairString + " does not exist.")
-    continueString = input("\nWould you like to input another pair? (y/n): ")
-    type(continueString)
-    if(continueString == "y"):
-        get_markets()
-    else:
-        bot()
 
 #basically GetMarkets except returns the value to the exterior function rather than printing it
 #MIGHT BE FUCKED UP
@@ -96,28 +49,6 @@ def retrievePrice( pairString ):
         return float(slicePrice)
     elif(pairIndex == -1 ):
         return 0
-
-
-#So you can test this out. When running the .bat for this file it should ask for a TradePairId.
-#Inputting the ID should output the ask price for that pair of currencies.
-#AskPrice just meaning the amount of the second coin for one of the first coin.
-#So inputting 4405 for example gives a rate of 1 HUSH to BLANK BTC. I think as of right now 1 hush is 0.00027491 BTC.
-#I'll work on more formatting later to output the actual pairing and more information.
-#I more or less just wanted to see if I could format the output using python.
-
-
-def bot():
-    methodString = input("\nPlease enter a command you would like to use: ")
-    if(methodString == "GetMarket"):
-        get_market()
-    elif(methodString == "GetMarkets"):
-        get_markets()
-    elif(methodString == "help"):
-        print("\nCurrent available commands are: \n'help' -Will display this list\n'exit' -Will terminate program\n'GetMarket' -Will retrieve current market asking price for a trade pair")
-    elif(methodString == "exit"):
-        sys.exit()
-    else:
-        print("\nPlease enter a valid command.")
 
 #The main screen print with some basic formatting logic
 def printLogScreen( ticker, lastPrice, SMA, lastBuyPrice, lastBuyQuant, waitBuy, lastSellPrice, lastSellQuant, waitSell, momentum):
@@ -233,10 +164,3 @@ def log( pairString ):
         time.sleep(30)
 
 log( "ETN/BTC" )
-
-
-#LEFTOVER CODE FROM TESTS THIS MORNING
-#api_query("GetMarket/1262")  #just another request test
-#api_query("GetMarkets")  #returns ALL markets regardless of trade pair id
-#api_query("GetMarketHistory/LTC_BTC/48")  #Returns all history of LTC to BTC exchanges within the past 48 hours. Without the 48 the default hour is 24.
-#This mother fucker works. We go frome here I guess (Create new files, I know its basic code but lets not fuck it up)
--- simpleHardCode.py
+++ simpleHardCode.py
@@ -1,84 +1,68 @@
 import requests
 import sys
 
-running = 1
-print("\nScript to find the current information about crypto.\nType 'help' to view valid commands.")
+class public_api:
+    def get_market():
+        tpiString = input("Please enter the TradePairId you would like to search for: ")
+        type(tpiString)
+        url = "https://www.cryptopia.co.nz/api/" + "GetMarket/" + tpiString
+        r = requests.get(url)
+        rString = r.text
+        lastIndex = rString.find("LastPrice")
+        buyIndex = rString.find("BuyVolume")
+        if(lastIndex != -1):
+            slicePrice = rString[lastIndex+11:buyIndex-2]
+            print("Last sold price of trade pair " + tpiString + " is: "  + slicePrice)
+        elif(lastIndex == -1):
+            slicePrice = rString[lastIndex+60:buyIndex-10]
+            print("TradePairId: " + slicePrice + "does not exist.")
+        continueString = input("\nWould you like to input another TradePairId? (y/n): ")
+        type(continueString)
+        if(continueString == "y"):
+            public_api.get_market()
+        else:
+            public_api.bot()
 
-#I'm going to make some changes to the methods. Primary so that we have specific methods for each API command instead
-#of having one large method that does everything. Maybe I'll clean it up later but this is mostly because
-#the one large method last time was fucking everything up. So I'm going to rename this method "api_query" to "get_market"
+    def get_markets():
+        pairString = input("\nPlease enter a pair in the format COIN/COIN to see their exchange rate: ")
+        type(pairString)
+        slashIndex = pairString.find("/")
+        coin1 = pairString[:slashIndex]
+        coin2 = pairString[slashIndex+1:]
+        url = "https://www.cryptopia.co.nz/api/GetMarkets"
+        r = requests.get(url)
+        rString = r.text
+        pairIndex = rString.find(pairString)
+        if(pairIndex != -1):
+            formattedText = rString[pairIndex:pairIndex+200]
+            lastPriceIndex = formattedText.find("LastPrice")
+            slicePrice = formattedText[(lastPriceIndex+11):(lastPriceIndex+20)]
+            print("The exchange rate for " + coin1 + " to " + coin2 + " is: " + slicePrice +"\nOr 1 " + coin1 + " to " + slicePrice + " " + coin2)
+        elif(pairIndex == -1):
+            print("The pair: " + pairString + " does not exist.")
+        continueString = input("\nWould you like to input another pair? (y/n): ")
+        type(continueString)
+        if(continueString == "y"):
+            public_api.get_markets()
+        else:
+            public_api.bot()
 
-def get_market():
-    tpiString = input("Please enter the TradePairId you would like to search for: ")
-    type(tpiString)
-    url = "https://www.cryptopia.co.nz/api/" + "GetMarket/" + tpiString
-    r = requests.get(url)
-    rString = r.text
-    lastIndex = rString.find("LastPrice")
-    buyIndex = rString.find("BuyVolume")
-    if(lastIndex != -1):
-        slicePrice = rString[lastIndex+11:buyIndex-2]
-        print("Last sold price of trade pair " + tpiString + " is: "  + slicePrice)
-    elif(lastIndex == -1):
-        slicePrice = rString[lastIndex+60:buyIndex-10]
-        print("TradePairId: " + slicePrice + "does not exist.")
-    continueString = input("\nWould you like to input another TradePairId? (y/n): ")
-    type(continueString)
-    if(continueString == "y"):
-        get_market()
-    else:
-        bot()
+    def help():
+        print("\nCurrent available commands are: \n")
+        print("'help' -Will display this list\n")
+        print("'exit' -Will terminate program\n")
+        print("'GetMarketId' -Will retrieve current market price for a trade pair Id\n")
+        print("'GetMarketPair' -Will retrieve current market asking price for a trade pair")
 
-def get_markets():
-    pairString = input("\nPlease enter a pair in the format COIN/COIN to see their exchange rate: ")
-    type(pairString)
-    slashIndex = pairString.find("/")
-    coin1 = pairString[:slashIndex]
-    coin2 = pairString[slashIndex+1:]
-    url = "https://www.cryptopia.co.nz/api/GetMarkets"
-    r = requests.get(url)
-    rString = r.text
-    pairIndex = rString.find(pairString)
-    if(pairIndex != -1):
-        formattedText = rString[pairIndex:pairIndex+200]
-        lastPriceIndex = formattedText.find("LastPrice")
-        slicePrice = formattedText[(lastPriceIndex+11):(lastPriceIndex+20)]
-        print("The exchange rate for " + coin1 + " to " + coin2 + " is: " + slicePrice +"\nOr 1 " + coin1 + " to " + slicePrice + " " + coin2)
-    elif(pairIndex == -1):
-        print("The pair: " + pairString + " does not exist.")
-    continueString = input("\nWould you like to input another pair? (y/n): ")
-    type(continueString)
-    if(continueString == "y"):
-        get_markets()
-    else:
-        bot()
-
-#So you can test this out. When running the .bat for this file it should ask for a TradePairId.
-#Inputting the ID should output the ask price for that pair of currencies.
-#AskPrice just meaning the amount of the second coin for one of the first coin.
-#So inputting 4405 for example gives a rate of 1 HUSH to BLANK BTC. I think as of right now 1 hush is 0.00027491 BTC.
-#I'll work on more formatting later to output the actual pairing and more information.
-#I more or less just wanted to see if I could format the output using python.
-
-
-def bot():
-    methodString = input("\nPlease enter a command you would like to use: ")
-    if(methodString == "GetMarketId"):
-        get_market()
-    elif(methodString == "GetMarketPair"):
-        get_markets()
-    elif(methodString == "help"):
-        print("\nCurrent available commands are: \n'help' -Will display this list\n'exit' -Will terminate program\n'GetMarketId' -Will retrieve current market price for a trade pair Id\n'GetMarketPair' -Will retrieve current market asking price for a trade pair ")
-    elif(methodString == "exit"):
-        sys.exit()
-    else:
-        print("\nPlease enter a valid command.")
-
-while running == 1:
-    bot()
-
-#LEFTOVER CODE FROM TESTS THIS MORNING
-#api_query("GetMarket/1262")  #just another request test
-#api_query("GetMarkets")  #returns ALL markets regardless of trade pair id
-#api_query("GetMarketHistory/LTC_BTC/48")  #Returns all history of LTC to BTC exchanges within the past 48 hours. Without the 48 the default hour is 24.
-#This mother fucker works. We go frome here I guess (Create new files, I know its basic code but lets not fuck it up)
+    def bot():
+        methodString = input("\nPlease enter a command you would like to use: ")
+        if(methodString == "GetMarketId"):
+            public_api.get_market()
+        elif(methodString == "GetMarketPair"):
+            public_api.get_markets()
+        elif(methodString == "help"):
+            public_api.help()
+        elif(methodString == "exit"):
+            sys.exit()
+        else:
+            print("\nPlease enter a valid command.")
