5886afba6cce78b5c22735dcb82e7f5350c3f9a6
Thomas-Macomber <Thomas.Macomber@gmail.com>
2018-02-15 22:08:05 -0500

    Hopefully made menus understandable even without comments

--- menus.py
+++ menus.py
@@ -5,9 +5,11 @@ currentMenu = 0
 previousMenu = 0
 
 currentMenus = []
-menus = ["Main Menu", "Help Menu", "Curriency Log"]
-menusMain = ["Help", "Log"]
-menusHelp = ["Log"]
+menus = ["Main Menu", "Help Menu", "Menu 3", "Menu 4"]
+menusMain = ["Help", "Menu 3", "Menu 4", "Exit"]
+menusHelp = ["HelpItem 1", "HelpItem 2", "Etc"]
+menusMenu3 = ["SubMenu3a","SubMenu3b"]
+menusMenu4 = ["SubMenu4a"]
 
 def printMenu( menu ):
     os.system('cls')
@@ -20,4 +22,8 @@ def printMenu( menu ):
         for i in range(0, len(menusHelp[])):
             print ( menusHelp[i] )
     elif menu == 2:
-        startBot()        
+        for i in range(0, len(menusMenu3[])):
+            print ( menusMenu3[i] )
+    elif menu == 3:
+        for i in range(0, len(menusMenu4[])):
+            print ( menusMenu4[i] )
